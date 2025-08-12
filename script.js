604418d59cc754a2be1d7a890e7189e2b4c53a6f
yashdoesntexist <learnhacksandtricks@gmail.com>
2025-08-12 00:40:18 -0700

    Everything finished except the winning function

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-let boxes = document.querySelectorAll(".box");
-let restart = document.getElementsByClassName("refresh");
+let boxes = document.querySelectorAll(".tab");
+let restart = document.querySelector(".refresh");
+
+let assignTurn = true;
+
+const winningPattern = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(assignTurn === true){
+            box.innerText = "X";
+            assignTurn = false;
+        } else if(assignTurn === false){
+            box.innerText = "O";
+            assignTurn = true;
+        } else{
+            console.log("I should not be here");
+        }
+    });
+});
+boxes.forEach((box) =>{
+    box.addEventListener("dblclick", () =>{
+        box.innerText = "";
+    })
+})
+restart.addEventListener("click", () =>{
+    boxes.forEach((e) =>{
+        e.innerText = "";
+    });
+ });
